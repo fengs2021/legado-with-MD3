c0f9c0764b1e2dbf06da1f980fa6dc758cc2615f
fengs <fengs@feishu>
2026-04-26 11:12:07 +0800

    fix(UI): 图标改用Icons.Default.Settings

--- app/src/main/java/io/legado/app/ui/main/my/MyScreen.kt
+++ app/src/main/java/io/legado/app/ui/main/my/MyScreen.kt
@@ -163,7 +163,7 @@ fun MyScreen(
                     )
                     ClickableSettingItem(
                         title = stringResource(R.string.ai_correction),
-                        imageVector = Icons.Default.Refresh,
+                        imageVector = Icons.Default.Settings,
                         onClick = {
                             onNavigate(
                                 PrefClickEvent.StartActivity(AICorrectionActivity::class.java)
