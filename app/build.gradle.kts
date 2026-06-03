82c8db9e9b822f11634f9e5473ef137b228054c3
fengs <fengs@feishu>
2026-06-04 00:34:05 +0800

    chore: 只编译arm64-v8a版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,8 +119,8 @@ android {
         abi {
             isEnable = true
             reset()
-            include("armeabi-v7a", "arm64-v8a")
-            isUniversalApk = true
+            include("arm64-v8a")
+            isUniversalApk = false
         }
     }
 
